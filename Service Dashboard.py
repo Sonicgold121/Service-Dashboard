233ccf2462a6c7a59455f965c099e1c4622a2c31
Sonicgold121 <77582196+Sonicgold121@users.noreply.github.com>
2025-09-08 14:05:41 -0700

    Update Service Dashboard.py

--- Service Dashboard.py
+++ Service Dashboard.py
@@ -156,7 +156,7 @@ def gsheet_update_wrapper(update_function, *args):
     try:
         scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
                  "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-        creds = ServiceAccountCredentials.from_json_keyfile_name(CREDS_FILE, scope)
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
         client = gspread.authorize(creds)
         spreadsheet = client.open(GSHEET_NAME)
         worksheet = spreadsheet.get_worksheet(WORKSHEET_INDEX)
