d8075ccb478957ea3314522eb2280daadf44f214
Sonicgold121 <77582196+Sonicgold121@users.noreply.github.com>
2025-09-08 13:41:11 -0700

    Update Laser Console Status Dashboard.py

--- pages/Laser Console Status Dashboard.py
+++ pages/Laser Console Status Dashboard.py
@@ -48,7 +48,7 @@ def load_data_from_google_sheet(
     try:
         scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
                  "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-        creds = ServiceAccountCredentials.from_json_keyfile_name(creds_file, scope)
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scopes)
         client = gspread.authorize(creds)
         spreadsheet = client.open(sheet_name)
         worksheet = spreadsheet.get_worksheet(worksheet_index)
