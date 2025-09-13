809245dfb378d9f3ce141de849df2a99dfa190d6
Sonicgold121 <77582196+Sonicgold121@users.noreply.github.com>
2025-09-12 20:16:09 -0700

    Update Ticket System.py

--- pages/Ticket System.py
+++ pages/Ticket System.py
@@ -5,7 +5,7 @@ import pandas as pd
 from logic import send_ticket_reply_and_log, update_ticket_status
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from logic import send_ticket_reply_and_log, update_ticket_status # <-- Add update_ticket_status here
+
 
 # --- Page Config ---
 st.set_page_config(page_title="Ticketing System", layout="wide")
@@ -108,4 +108,5 @@ if sheet:
                                 load_tickets.clear()
                             else:
                                 st.error(message)
+
 
